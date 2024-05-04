f7f09e999eac71466165c9e55ca8ef7a9e1b2338
Lucandroid70 <43816108+lucandroid70@users.noreply.github.com>
2024-05-04 12:27:56 +0200

    Create immich_rocky_centos.sh

---
+++ immich_rocky_centos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# Funzione per l'installazione su Rocky o CentOs Linux
+# Script creato da Luca Droi :) 
+    read -p "Premi enter per aggiungere i repository Docker e installare l'ultima versione di Immich!"
+    sudo dnf upgrade -y
+    sudo dnf install -y avahi
+    sudo dnf install -y dnf-plugins-core
+    sudo dnf install bash-completion -y
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo dnf install -y docker-ce docker-ce-cli containerd.io
+    sudo dnf install docker-compose-plugin
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo dnf install -y docker-compose
+
+
+# Installa Immich
+sudo curl -o- https://raw.githubusercontent.com/immich-app/immich/main/install.sh | sudo bash
