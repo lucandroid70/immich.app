c22a6e6f60873a59cd8d659fb3f6a8d2636f6d8b
Lucandroid70 <43816108+lucandroid70@users.noreply.github.com>
2024-05-04 19:12:32 +0200

    Update immich_rocky_centos.sh

--- immich_rocky_centos.sh
+++ immich_rocky_centos.sh
@@ -5,10 +5,17 @@
 # Script creato da Luca Droi :) 
     read -p "Premi enter per aggiungere i repository Docker e installare l'ultima versione di Immich!"
     sudo dnf upgrade -y
+    # Installation epel source (also called repository)
+    sudo dnf -y install epel-release
+    # Generate cache
+    sudo dnf makecache
+    # Install htop
+    sudo dnf install htop bmon nload   bash-completion  -y
     sudo dnf install -y avahi
     sudo dnf install -y dnf-plugins-core
     sudo dnf install bash-completion -y
-    sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    
+      sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     sudo dnf install -y docker-ce docker-ce-cli containerd.io
     sudo dnf install docker-compose-plugin
     sudo systemctl start docker
